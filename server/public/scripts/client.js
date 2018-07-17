759b744af0f65ec2868d88553903a3167b6db9d3
Nathan Carroll <ntc238@gmail.com>
2018-07-16 22:47:28 -0500

    draw row function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,67 +1,84 @@
 const app = angular.module('CA-app', []);
 
 app.controller('CA-controller', function(){
-    console.log('ca-controller started');
-    let self=this;
+    let self = this;
+
+    self.rulebook = [];
+    self.neighbors = 3;
+    self.states = 2;
+    self.currentRow = 0;
 
     c = document.getElementById('myCanvas');
     ctx = c.getContext('2d');
     console.log(screen.width);
 
-    c.width = screen.width / 1.4;
-    c.height = screen.height / 1.4;
+    c.width = 1001;
+    c.height = 600;
 
-    let imgData = ctx.createImageData(c.width, 1);
-    console.log(imgData.data.length);
+    let firstRow = new Array(c.width).fill(0);
+    firstRow[501] = 1;
 
-    for (let i=0; i<imgData.data.length; i+=4){
-        imgData.data[i] = 0;
-        imgData.data[i+1] = 0;
-        imgData.data[i+2] = 255;
-        imgData.data[i+3] = 255;
-    }
+    let imgData = ctx.createImageData(c.width, c.height);
+    console.log(c.width, imgData.data.length);
 
-    ctx.putImageData(imgData,0,0);
+    ctx.putImageData(imgData,0,self.currentRow);
 
-    document.getElementById('goButton').addEventListener('click', generateRule);
-
-    let rulebook = [];
-    let neighbors = 3;
-    let states = 2;
-
-    function generateRule(event, rule=90){
-        console.log(`generate rule function called with ${neighbors} neighbors and ${states} states`);
-        console.log(states**(states**neighbors));
-        if (rule > states**(states**neighbors)){
+    self.generateRule = function(event, rule=90){
+        console.log(`generate rule function called with ${self.neighbors} neighbors and ${self.states} states`);
+        if (rule > self.states**(self.states**self.neighbors)){
             console.log('invalid rule number! try again');
             return;
         }
-        const ruleString = leadingZeros(rule.toString(states), states**neighbors);
-        rulebook = Array.from(ruleString);
-        console.log(rulebook);
-        console.log(nextRow([0,0,0,0,0,1,0,0,0,0,0]));
+        const ruleString = self.leadingZeros(rule.toString(self.states), self.states**self.neighbors);
+        self.rulebook = Array.from(ruleString);
+        console.log(self.rulebook);
+        console.log(self.nextRow(firstRow));
     }
 
-    function leadingZeros(numberString, desiredLength){
+    self.leadingZeros = function(numberString, desiredLength){
         while (numberString.length < desiredLength){
             numberString = '0' + numberString;
         }
         return numberString;
     }
 
-    function applyRule(){
+    self.applyRule = function(){
         let codon = Array.from(arguments).join('');
-        return rulebook[parseInt(codon, states)];
+        return self.rulebook[parseInt(codon, self.states)];
     }
 
-    function nextRow(previousRow){
+    self.nextRow = function(previousRow){
         let nextRow = [];
-        nextRow.push(applyRule(previousRow[previousRow.length-1], previousRow[0], previousRow[1]))
+        nextRow.push(self.applyRule(previousRow[previousRow.length-1], previousRow[0], previousRow[1]))
         for (i=1; i< previousRow.length-1; i++){
-            nextRow.push(applyRule(previousRow[i-1], previousRow[i], previousRow[i+1]));
+            nextRow.push(self.applyRule(previousRow[i-1], previousRow[i], previousRow[i+1]));
         }
-        nextRow.push(applyRule(previousRow[previousRow.length-2], previousRow[previousRow.length-1], previousRow[0]));
+        nextRow.push(self.applyRule(previousRow[previousRow.length-2], previousRow[previousRow.length-1], previousRow[0]));
         // console.log(nextRow);
         return nextRow;
+    }
+
+    self.drawRow = function(row){
+        let rgbRow = new Array(row.length*4).fill(0);
+        for (let i=0; i < row.length; i++){
+            if (row[i] === 0){
+                // BLUE
+                rgbRow[i*4+2] = 255;
+            } else if (row[i] === 1){
+                // RED
+                rgbRow[i*4] = 255;
+            } else if (row[i] === 2){
+                // YELLOW
+                rgbRow[i*4] = 255;
+                rgbRow[i*4+1] = 255;
+            } else if (row[i] = 3){
+                // GREEN
+                rgbRow[i*4+1] = 255;
+                rgbRow[i*4+2] = 255;
+            }
+            rgbRow[i*4+3] = 255;
+        }
+        return rgbRow;
+
     }
 })
