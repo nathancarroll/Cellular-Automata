56770caa9ff292d6ff6b880f7b7cdee2394f3986
Nathan Carroll <ntc238@gmail.com>
2018-07-17 10:10:11 -0500

    change rows from arrays to string

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,28 +3,33 @@ const app = angular.module('CA-app', []);
 app.controller('CA-controller', function(){
     let self = this;
 
-    self.rulebook = [];
+    // Neighbors and states are hardcoded for now, will be variables in the future
+    self.ruleString = '';
     self.neighbors = 3;
     self.states = 2;
-    self.currentRow = 0;
+    self.rowNumber = 0;
 
+    // Initialize your canvas object
     c = document.getElementById('myCanvas');
     ctx = c.getContext('2d');
-    console.log(screen.width);
 
     c.width = 1001;
     c.height = 600;
 
-    self.generateRule = function(event, rule=90){
+    // The first row is currently hardcoded to be all zeroes except for the center unit
+    self.currentRow = new Array(c.width).fill('0');
+    self.currentRow[501] = '1';
+    self.currentRow = self.currentRow.join('');
+    console.log(self.currentRow);
+
+    self.generateRule = function(rule=90){
         console.log(`generate rule function called with ${self.neighbors} neighbors and ${self.states} states`);
         if (rule > self.states**(self.states**self.neighbors)){
             console.log('invalid rule number! try again');
             return;
         }
-        const ruleString = self.leadingZeros(rule.toString(self.states), self.states**self.neighbors);
-        self.rulebook = Array.from(ruleString);
-        console.log(self.rulebook);
-        console.log(self.nextRow(firstRow));
+        self.ruleString = self.leadingZeros(rule.toString(self.states), self.states**self.neighbors);
+        console.log(self.ruleString);
     }
 
     self.leadingZeros = function(numberString, desiredLength){
@@ -36,53 +41,66 @@ app.controller('CA-controller', function(){
 
     self.applyRule = function(){
         let codon = Array.from(arguments).join('');
-        return self.rulebook[parseInt(codon, self.states)];
+        return self.ruleString.charAt(parseInt(codon, self.states));
     }
 
     self.nextRow = function(previousRow){
-        let nextRow = [];
-        nextRow.push(self.applyRule(previousRow[previousRow.length-1], previousRow[0], previousRow[1]))
+        // console.log('current row', previousRow);
+        let nextRow = '';
+        nextRow += self.applyRule(previousRow.charAt(previousRow.length-1), previousRow.charAt(0), previousRow.charAt(1));
         for (i=1; i< previousRow.length-1; i++){
-            nextRow.push(self.applyRule(previousRow[i-1], previousRow[i], previousRow[i+1]));
+            nextRow += self.applyRule(previousRow.charAt(i-1), previousRow.charAt(i), previousRow.charAt(i+1))
         }
-        nextRow.push(self.applyRule(previousRow[previousRow.length-2], previousRow[previousRow.length-1], previousRow[0]));
-        // console.log(nextRow);
+        nextRow + self.applyRule(previousRow.charAt(previousRow.length-2), previousRow.charAt(previousRow.length-1), previousRow.charAt(0));
+        // console.log('next row:', nextRow);
         return nextRow;
     }
 
     self.drawRow = function(row){
         let rgbRow = ctx.createImageData(c.width, 1);
-        for (let i=0; i<rgbRow.data.length; i+=4){
-            rgbRow.data[i] = 255;
-            rgbRow.data[i+1] = 0;
-            rgbRow.data[i+2] = 0;
-            rgbRow.data[i+3] = 255;
-        }
-        // for (let i=0; i < row.length; i++){
-        //     if (row[i] === 0){
-        //         // BLUE
-        //         rgbRow[i*4+2] = 255;
-        //     } else if (row[i] === 1){
-        //         // RED
-        //         rgbRow[i*4] = 255;
-        //     } else if (row[i] === 2){
-        //         // YELLOW
-        //         rgbRow[i*4] = 255;
-        //         rgbRow[i*4+1] = 255;
-        //     } else if (row[i] = 3){
-        //         // GREEN
-        //         rgbRow[i*4+1] = 255;
-        //         rgbRow[i*4+2] = 255;
-        //     }
-        //     rgbRow[i*4+3] = 255;
+        // console.log(rgbRow.data);
+        // for (let i=0; i<rgbRow.data.length; i+=4){
+        //     rgbRow.data[i] = 255;
+        //     rgbRow.data[i+1] = 0;
+        //     rgbRow.data[i+2] = 0;
+        //     rgbRow.data[i+3] = 255;
         // }
-
-        ctx.putImageData(rgbRow,0,self.currentRow);
-        self.currentRow++;
+        // row = Array.from(row);
+        console.log(row, row.length);
+        for (let i=0; i < row.length; i++){
+            if (row[i] === 0){
+                // BLUE
+                rgbRow.data[i*4+2] = 255;
+            } else if (row[i] === 1){
+                // RED
+                rgbRow.data[i*4] = 255;
+            } else if (row[i] === 2){
+                // YELLOW
+                rgbRow.data[i*4] = 255;
+                rgbRow.data[i*4+1] = 255;
+            } else if (row[i] = 3){
+                // GREEN
+                rgbRow.data[i*4+1] = 255;
+                rgbRow.data[i*4+2] = 255;
+            }
+            rgbRow.data[i*4+3] = 255;
+        }
+        // console.log(rgbRow.data);
+        ctx.putImageData(rgbRow,0,self.rowNumber);
     }
 
-    let firstRow = new Array(c.width).fill(0);
-    firstRow[501] = 1;
-    self.drawRow(firstRow);
+    // self.drawRow(firstRow);
+
+    self.populate = function(){
+        while (self.rowNumber < 100){
+            self.drawRow(self.currentRow);
+            self.currentRow = self.nextRow(self.currentRow)
+            self.rowNumber++;
+        }
+        console.log(self.currentRow);
+    }
+
+    self.generateRule();
+    self.populate();
     
 })
