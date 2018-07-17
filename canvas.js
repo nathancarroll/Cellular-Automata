37f1c9defa81125438832a055e1d19227e13870a
Nathan Carroll <ntc238@gmail.com>
2018-07-16 21:40:30 -0500

    nextRow function from rulebook

--- canvas.js
+++ canvas.js
@@ -2,7 +2,7 @@ let rulebook = [];
 let neighbors = 3;
 let states = 2;
 
-function generateRule(event, rule=300){
+function generateRule(event, rule=90){
     console.log(`generate rule function called with ${neighbors} neighbors and ${states} states`);
     console.log(states**(states**neighbors));
     if (rule > states**(states**neighbors)){
@@ -11,7 +11,8 @@ function generateRule(event, rule=300){
     }
     const ruleString = leadingZeros(rule.toString(states), states**neighbors);
     rulebook = Array.from(ruleString);
-    console.log(rulebook); 
+    console.log(rulebook);
+    console.log(nextRow([0,0,0,0,0,1,0,0,0,0,0]));
 }
 
 function leadingZeros(numberString, desiredLength){
@@ -23,6 +24,17 @@ function leadingZeros(numberString, desiredLength){
 
 function applyRule(){
     let codon = Array.from(arguments).join('');
-    return ruleBook[parseInt(codon, states)];
+    return rulebook[parseInt(codon, states)];
+}
+
+function nextRow(previousRow){
+    let nextRow = [];
+    nextRow.push(applyRule(previousRow[previousRow.length-1], previousRow[0], previousRow[1]))
+    for (i=1; i< previousRow.length-1; i++){
+        nextRow.push(applyRule(previousRow[i-1], previousRow[i], previousRow[i+1]));
+    }
+    nextRow.push(applyRule(previousRow[previousRow.length-2], previousRow[previousRow.length-1], previousRow[0]));
+    // console.log(nextRow);
+    return nextRow;
 }
 
